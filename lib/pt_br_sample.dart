c87e00977cbfa93a19a7ba8cd1e8b6e9f1ec52bb
Douglas Gonçalves de Souza <ti.dougg@gmail.com>
2026-04-24 13:31:15 -0300

    Reinicia o projeto

--- lib/pt_br_sample.dart
+++ lib/pt_br_sample.dart
@@ -1,14 +1,11 @@
+import 'package:system_language/books/book.dart';
 
-import 'package:application_language/books/book.dart';
-
-class PtBrSample extends Book
-{
-  PtBrSample()
-  {
+class PtBrSample extends Book {
+  PtBrSample() {
     super.texts = {
       'pt_br': 'This language for pt_br.',
       'language': 'pt_br',
-      'molded_sample': 'A saída deve ser >:1.'
+      'molded_sample': 'A saída deve ser >:1.',
     };
   }
 }
---
+++ lib/system_language_lib.dart
@@ -0,0 +1,4 @@
+library;
+
+export 'package:system_language/system_language.dart';
+export 'package:system_language/books/book.dart';
